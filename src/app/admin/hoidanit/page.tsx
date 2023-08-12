796e55acbae0b3ab80fea022fbd26a6e889bf52c
Joker3052 <phuonghoangthanh3052@gmail.com>
2023-08-13 04:41:55 +0700

    123

---
+++ src/app/admin/hoidanit/page.tsx
@@ -0,0 +1,13 @@
+// "use client"
+// const HoiDanIT = () => {
+//     return (
+//         <>
+//             hoidanit admin
+//         </>
+//     )
+// }
+
+// export default HoiDanIT;
+export default function Page() {
+    return <h1>Hello, Dashboard Page!</h1>
+  }
